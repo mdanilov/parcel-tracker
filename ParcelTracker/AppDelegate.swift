39380e45aa88561aa5ea27793fbe5945efb892d3
Maxim Danilov <mdanilov@luxoft.com>
2018-11-23 09:51:24 +0300

    Initial Commit

---
+++ ParcelTracker/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ParcelTracker
+//
+//  Created by Максим Данилов on 23/11/2018.
+//  Copyright © 2018 Maxim Danilov. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ParcelTracker/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ParcelTracker
+//
+//  Created by Максим Данилов on 23/11/2018.
+//  Copyright © 2018 Maxim Danilov. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
